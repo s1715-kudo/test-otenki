1270216819f66f919d78e7f64e0d15b4a694ed75
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 01:44:19 +0900

    fix } error

--- map.js
+++ map.js
@@ -41,7 +41,7 @@ function createInitMap(){
 	myPolygon=new google.maps.Polygon({path:polygon_list,strokeColor:polygon_color,fillColor:polygon_color});
 	myPolygon.setMap(map);
 	
-	if(urlPolygonAuteSave==1)
+	if(urlPolygonAuteSave==1){
 		for(var i=0;i<polygon_json_list.length;i++){
 			polygon_list.push(new google.maps.LatLng(polygon_json_list[i]["lat"],polygon_json_list[i]["lng"]))
 			click_marker = new google.maps.Marker({
