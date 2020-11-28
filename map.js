aa550f6f617f178f8a8e45a60edfa5eacd034df6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 01:57:32 +0900

    fix polygon_json_list error

--- map.js
+++ map.js
@@ -30,8 +30,9 @@ function viewInitMap(){
 
 function createInitMap(){
 	var click_marker;
+	var polygon_json_list;
 	if(urlPolygonAuteSave==1){
-		var polygon_json_list=JSON.parse(Cookies.get("polygon"));
+		polygon_json_list=JSON.parse(Cookies.get("polygon"));
 		if(polygon_json_list==null)polygon_json_list=[]
 	}
 	map = new google.maps.Map(document.getElementById('map'), Options);
