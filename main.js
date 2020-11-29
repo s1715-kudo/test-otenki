14b96d6bfaf0aad01f862640bb82275a24b5e1a9
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 10:52:31 +0900

    polygon log

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ locate={lat:setKeyinit("map_lat",locate["lat"]),lng:setKeyinit("map_lng",locate[
 window.onload = function() {
 	$("body").css("background","#"+setKeyinit('background_color','98FB98'));
 	document.getElementById('polygon_area').innerHTML=google.maps.geometry.spherical.computeArea(polygon_list).toFixed(2)+"㎡";
-	if(urlPolygonAutoSave==1)document.getElementById('autosave_text').innerHTML="前回作成したデータを読み込みました。"
+	if(urlPolygonAutoSave==1)document.getElementById('autosave_text').innerHTML="<p>前回作成したデータを読み込みました。</p>"
 };
 
 function getDay(str) {
