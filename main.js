cdfb245b19a279642f9b5ef3040f2dac4dd1c363
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 11:05:11 +0900

    comment

--- main.js
+++ main.js
@@ -16,12 +16,6 @@ $.getJSON(amedas_url, function(data){
 $.ajaxSetup({async: true});
 locate={lat:setKeyinit("map_lat",locate["lat"]),lng:setKeyinit("map_lng",locate["lng"])}
 
-window.onload = function() {
-	$("body").css("background","#"+setKeyinit('background_color','98FB98'));
-	document.getElementById('polygon_area').innerHTML=google.maps.geometry.spherical.computeArea(polygon_list).toFixed(2)+"㎡";
-	if(urlPolygonAutoSave==1)document.getElementById('autosave_text').innerHTML="<p>前回作成したデータを読み込みました。</p>"
-};
-
 function getDay(str) {
 	var dw=new Date (str).getDay();
 	var list=["日","月","火","水","木","金","土"];
