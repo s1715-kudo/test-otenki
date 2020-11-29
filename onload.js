459e56f882ce06c83bcba27098ead6d2918e8fda
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 11:31:08 +0900

    polygon_list length log

--- onload.js
+++ onload.js
@@ -1,5 +1,5 @@
 window.onload = function() {
 	$("body").css("background","#"+setKeyinit('background_color','98FB98'));
 	document.getElementById('polygon_area').innerHTML=google.maps.geometry.spherical.computeArea(polygon_list).toFixed(2)+"㎡";
-	if(urlPolygonAutoSave==1)document.getElementById('autosave_text').innerHTML="<p>前回作成したデータを読み込みました。</p>"
+	if(urlPolygonAutoSave==1 && polygon_list.length!=0)document.getElementById('autosave_text').innerHTML="<p>前回作成したデータを読み込みました。</p>"
 };
