def4dd3e9deebbe91a9f81c3942644e9355a76bd
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 17:59:46 +0900

    onload

--- onload.js
+++ onload.js
@@ -5,14 +5,13 @@ $(window).on('load', function(){
 	var get_polygon_color=Cookies.get("polygon_color");
 	if(get_comment_text==null || get_comment_text===undefined)get_comment_text="";
 	if(get_polygon_color==null || get_polygon_color===undefined)get_polygon_color="#000000";
-	
-	console.log(get_polygon_color);
 	if(urlCommentAutoSave==1 && (polygon_list.length!=0 || get_comment_text.length!=0 || get_polygon_color!="#000000")){
 		var result=confirm("前回作成したコメントのデータがあります。ロードしますか？");
 		if(result){
 			document.getElementById('autosave_text').innerHTML="<p>前回作成したデータを読み込みました。</p><input type='button' value='リセット' onclick='comment_reset()'>"
 			document.forms.mapForm.formComment.value=get_comment_text;
 			polgon_color=get_polygon_color;
+			console.log(polygon_color);
 			myPolygon.setOptions({ fillColor:polygon_color,strokeColor:polygon_color});
 			var polygon_center=polygon_list_center();
 			if(polygon_center!=null)map.panTo(polygon_center)
