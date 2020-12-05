9442d6aea33edcf908694cb4aa2a5da2a3808cc8
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 16:17:18 +0900

    reload

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-window.onload = function() {
-	$(".circles").css("top","100px");
-}
-
 $(window).scroll(function () {
 	if($(".site_fooder").height()+$(".site_fooder").offset().top>$(this).height()+$(this).scrollTop()){
 		$("#area").css("top",$(this).scrollTop());
