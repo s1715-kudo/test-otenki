76be24939485caad5480833c5a2bd710b8e085c1
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 12:59:12 +0900

    background

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+	$(".circles").css("top","100px");
+}
+
 $(window).scroll(function () {
 	if($(".site_fooder").height()+$(".site_fooder").offset().top>$(this).height()+$(this).scrollTop()){
 		$("#area").css("top",$(this).scrollTop());
